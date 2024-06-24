b3d0998da10a201c138e8f097643ea2a00172044
Adebanjo Joshua Ibukunoluwa <36772552+Orthority@users.noreply.github.com>
2024-06-24 10:55:11 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,474 @@
+
+var quiz = {
+"JS": [
+ {
+            "id": 1,
+            "question": "A 35 year old woman is considered infertile after _________________ of trying to conceive.",
+            "options": [
+                {
+                    "a": "1 month",
+                    "b": "2 months",
+                    "c": "4 months",
+                    "d": "6 months"
+                }
+            ],
+            "answer": "1 month",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 2,
+            "question": "In in vitro fertilization (IVF) is a simple cost effective procedure for infertile couples.",
+            "options": [
+                {
+                    "a": "True",
+                    "b": "False"
+                }
+            ],
+            "answer": "False",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 3,
+            "question": "If a couple is infertile, this means the couple _____________________?",
+            "options": [
+                {
+                    "a": "will never be able to have children",
+                    "b": "could not conceive after 12 months of unprotected sex",
+                    "c": "should look into adoption",
+                    "d": "All of the above"
+                }
+            ],
+            "answer": "All of the above",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 4,
+            "question": "Which sexually transmitted disease can result in infertility in women?",
+            "options": [
+                {
+                    "a": "Human papillomavirus (HPV)",
+                    "b": "Genital Herpes",
+                    "c": "Pelvic Inflammatory disease",
+                    "d": "All of the above"
+                }
+            ],
+            "answer": "Pelvic Inflammatory disease",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 5,
+            "question": "Women should avoid pregnancy for three months after discontinuing birth control pills.",
+            "options": [
+                {
+                    "a": "True",
+                    "b": "False"
+                }
+            ],
+            "answer": "False",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 6,
+            "question": "_______________? Is a major factor for infertility in women.",
+            "options": [
+                {
+                    "a": "Age",
+                    "b": "weight",
+                    "c": "Anovulation",
+                    "d": "All of the above"
+                }
+            ],
+            "answer": "All of the above",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 7,
+            "question": "Men and women are equally likely to have infertility problems?",
+            "options": [
+                {
+                    "a": "True",
+                    "b": "False"
+                }
+            ],
+            "answer": "True",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 8,
+            "question": "Women who are trying to conceive should boost their intake of?",
+            "options": [
+                {
+                    "a": "Nickel",
+                    "b": "Lycopene",
+                    "c": "Potassium",
+                    "d": "Folic acid"
+                }
+            ],
+            "answer": "Folic acid",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 9,
+            "question": "The most fertile days of a woman's cycle can vary from months to months.",
+            "options": [
+                {
+                    "a": "True",
+                    "b": "False"
+                }
+            ],
+            "answer": "True",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 10,
+            "question": "Men do not experience age-related decrease in infertility.",
+            "options": [
+                {
+                    "a": "True",
+                    "b": "False"
+                }
+            ],
+            "answer": "False",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 11,
+            "question": "At which stage of the uterine cycle does the menstrual phase occur?",
+            "options": [
+                {
+                    "a": "1-15 days",
+                    "b": "5-14 days",
+                    "c": "14-28 days"
+                }
+            ],
+            "answer": "1-15 days",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 12,
+            "question": "Where is gonadotrophin-releasing hormone produced?",
+            "options": [
+                {
+                    "a": "Anterior pituitary gland",
+                    "b": "Posterior pituitary gland",
+                    "c": "Hypothalamus",
+                    "d": "Adrenal gland"
+                }
+            ],
+            "answer": "Hypothalamus",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 13,
+            "question": "At which stage in the uterine cycle does the secretory phase occur?",
+            "options": [
+                {
+                    "a": "1-5 Days",
+                    "b": "5-14 days",
+                    "c": "14-28 days"
+                }
+            ],
+            "answer": "14-28 days",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 14,
+            "question": "At which point in the menstrual cycle is a woman most fertile?",
+            "options": [
+                {
+                    "a": "1-5 days",
+                    "b": "9-16 days",
+                    "c": "17-21 days",
+                    "d": "22-28 days"
+                }
+            ],
+            "answer": "1-5 days",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 15,
+            "question": "Which of the following is the primary source of progesterone in the later stage of pregnancy?",
+            "options": [
+                {
+                    "a": "Foetus",
+                    "b": "Endometrium",
+                    "c": "Corpus luteum",
+                    "d": "Placenta"
+                }
+            ],
+            "answer": "Foetus",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 16,
+            "question": "Which of the following is the function of luteinizing hormone (LH)?",
+            "options": [
+                {
+                    "a": "formation and maintenance of the corpus luteum",
+                    "b": "thinning of the graffian follicles membrane",
+                    "c": "stimulation of follicle development",
+                    "d": "A and B"
+                }
+            ],
+            "answer": "A and B",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 17,
+            "question": "Which of the following symptoms indicate that a woman is about to ovulate?",
+            "options": [
+                {
+                    "a": "Decrease in basal body temperature",
+                    "b": "Increase in basal body temperature",
+                    "c": "Thickening of cervical mucous",
+                    "d": "Thinning of cervical mucous"
+                }
+            ],
+            "answer": "Increase in basal body temperature",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 18,
+            "question": "At which stage in the uterine cycle does the proliferative phase occur?",
+            "options": [
+                {
+                    "a": "1-15 days",
+                    "b": "5-14 days",
+                    "c": "15–28 days"
+                }
+            ],
+            "answer": "5-14 days",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 19,
+            "question": "Which of the following hormone is the corpus luteum responsible for producing?",
+            "options": [
+                {
+                    "a": "Oestrogen",
+                    "b": "Progesterone",
+                    "c": "Follicle stimulating hormone",
+                    "d": "Luteinizing hormone"
+                }
+            ],
+            "answer": "Progesterone",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 20,
+            "question": "Where are luteinizing hormone (LH) and follicle stimulating hormone (FSH) produced?",
+            "options": [
+                {
+                    "a": "Hypothalamus",
+                    "b": "Anterior pituitary gland",
+                    "c": "Posterior pituitary gland",
+                    "d": "Adrenal gland"
+                }
+            ],
+            "answer": "Anterior pituitary gland",
+            "score": 0,
+            "status": ""
+        },
+        {
+            "id": 21,
+            "question": "Which of the following are function of progesterone during menstrual cycle?",
+            "options": [
+                {
+                    "a": "It causes the endometrium to become more receptive to implantation of a fertilized ovum",
+                    "b": "It inhibits the production of luteinizing hormone (LH) and follicle stimulating hormone (FSH) via negative feedback",
+                    "c": "Increase in basal body temperature",
+                    "d": "All of the above"
+                }
+            ],
+            "answer": "All of the above",
+            "score": 0,
+            "status": ""
+        }
+]
+}
+var quizApp = function () {
+    this.score = 0;
+    this.qno = 1;
+    this.currentque = 0;
+    this.answeredQuestions = [];
+    this.totalque = quiz.JS.length;
+    this.timer = 10800; 
+
+    this.displayQuiz = function (cque) {
+        this.currentque = cque;
+        if (this.currentque < this.totalque) {
+            $("#tque").html(this.totalque);
+            $("#previous").attr("disabled", false);
+            $("#next").attr("disabled", false);
+            $("#qid").html(quiz.JS[this.currentque].id + '.');
+            $("#question").html(quiz.JS[this.currentque].question);
+            $("#question-options").html("");
+            for (var key in quiz.JS[this.currentque].options[0]) {
+                if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+                    $("#question-options").append(
+                        "<div class='form-check option-block'>" +
+                        "<label class='form-check-label'>" +
+                        "<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+                        quiz.JS[this.currentque].options[0][key] +
+                        "</span></label>"
+                    );
+                }
+            }
+        }
+        if (this.currentque <= 0) {
+            $("#previous").attr("disabled", true);
+        }
+        if (this.currentque >= this.totalque) {
+            $('#next').attr('disabled', true);
+            for (var i = 0; i < this.totalque; i++) {
+                this.score = this.score + quiz.JS[i].score;
+            }
+            return this.showResult(this.score);
+        }
+        this.highlightQuestionButtons();
+    }
+
+    this.showResult = function (scr) {
+        $("#result").addClass('result');
+        $("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + this.totalque + "</h1>");
+        for (var j = 0; j < this.totalque; j++) {
+            var res;
+            if (quiz.JS[j].score == 0) {
+                res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+            } else {
+                res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+            }
+            $("#result").append(
+                '<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+                '<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+                '<div class="last-row"><b>Score:</b> &nbsp;' + res +
+                '</div>'
+            );
+        }
+        this.updateSummary();
+    }
+
+    this.checkAnswer = function (option) {
+        var answer = quiz.JS[this.currentque].answer;
+        option = option.replace(/</g, "&lt;") //for <
+        option = option.replace(/>/g, "&gt;") //for >
+        option = option.replace(/"/g, "&quot;")
+        if (option == quiz.JS[this.currentque].answer) {
+            if (quiz.JS[this.currentque].score == "") {
+                quiz.JS[this.currentque].score = 1;
+                quiz.JS[this.currentque].status = "correct";
+            }
+        } else {
+            quiz.JS[this.currentque].status = "wrong";
+        }
+        if (!this.answeredQuestions.includes(this.currentque)) {
+            this.answeredQuestions.push(this.currentque);
+        }
+        this.updateSummary();
+        this.highlightQuestionButtons();
+    }
+
+    this.changeQuestion = function (cque) {
+        this.currentque = this.currentque + cque;
+        this.displayQuiz(this.currentque);
+    }
+
+    this.updateSummary = function () {
+        $("#answered").text(this.answeredQuestions.length);
+        $("#unanswered").text(this.totalque - this.answeredQuestions.length);
+    }
+
+    this.startTimer = function () {
+        var self = this;
+        var timerInterval = setInterval(function () {
+            self.timer--;
+            var minutes = Math.floor(self.timer / 60);
+            var seconds = self.timer % 60;
+            $("#timer").text(minutes + ":" + (seconds < 10 ? '0' : '') + seconds);
+
+            if (self.timer <= 0) {
+                clearInterval(timerInterval);
+                self.showResult(self.score);
+            }
+        }, 1000);
+    }
+
+    this.createQuestionButtons = function () {
+        var questionList = $("#question-list");
+        for (var i = 0; i < this.totalque; i++) {
+            var button = $('<button/>', {
+                text: i + 1,
+                id: 'qbtn' + i,
+                click: function () {
+                    var qIndex = parseInt($(this).text()) - 1;
+                    quizAppObj.changeQuestion(qIndex - quizAppObj.currentque);
+                }
+            });
+            button.addClass('btn btn-danger');
+            questionList.append(button);
+        }
+    }
+
+    this.highlightQuestionButtons = function () {
+        for (var i = 0; i < this.totalque; i++) {
+            var button = $('#qbtn' + i);
+            if (this.answeredQuestions.includes(i)) {
+                button.removeClass('btn-danger').addClass('btn-success');
+            } else {
+                button.removeClass('btn-success').addClass('btn-danger');
+            }
+        }
+    }
+}
+
+var jsq = new quizApp();
+var selectedopt;
+jsq.displayQuiz(0);
+jsq.startTimer();
+jsq.createQuestionButtons();
+
+$(document).ready(function () {
+    $(document).on('change', 'input[name=option]', function () {
+        selectedopt = $(this).val();
+    });
+
+    $('#next').click(function (e) {
+        e.preventDefault();
+        if (selectedopt) {
+            jsq.checkAnswer(selectedopt);
+        }
+        jsq.changeQuestion(1);
+    });
+
+    $('#previous').click(function (e) {
+        e.preventDefault();
+        if (selectedopt) {
+            jsq.checkAnswer(selectedopt);
+        }
+        jsq.changeQuestion(-1);
+    });
+
+    $('#submit').click(function (e) {
+        e.preventDefault();
+        jsq.showResult(jsq.score);
+    });
+});
